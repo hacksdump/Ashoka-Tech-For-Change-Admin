415e69791b7ac1aed0537ee7d932707ef02fcd11
Harshit Maurya <hmaurya999@gmail.com>
2019-12-19 10:22:45 +0530

    inject saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from 'reducers';
+import createSagaMiddleware from 'redux-saga';
+import firebaseSaga from 'sagas/firebaseSaga';
 
-const store = createStore(rootReducer);
+const initializeSagaMiddleware = createSagaMiddleware();
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, storeEnhancers(
+  applyMiddleware(initializeSagaMiddleware)
+));
+
+initializeSagaMiddleware.run(firebaseSaga);
 
 export default store;
